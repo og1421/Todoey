b04c23333a52c9499015d4a4cbec4810a59d8e31
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-07-21 23:39:29 -0300

    ultimo att

--- Todoey/Controller/ToDoListViewController.swift
+++ Todoey/Controller/ToDoListViewController.swift
@@ -6,111 +6,146 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in:.userDomainMask).first?.appendingPathComponent("Items.plist")
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Teste1"
-        itemArray.append(newItem)
+//        let newItem = Item()
+//        newItem.title = "Teste1"
+//        itemArray.append(newItem)
+//
+//        let newItem2 = Item()
+//        newItem2.title = "Teste2"
+//        itemArray.append(newItem2)
+//
+//        let newItem3 = Item()
+//        newItem3.title = "Teste3"
+//        itemArray.append(newItem3)
         
-        let newItem2 = Item()
-        newItem2.title = "Teste2"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Teste3"
-        itemArray.append(newItem3)
-        
-        loadItems()
+//        loadItems()
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
+    //MARK: - Tableview Datasource Methods
         
-        let item = itemArray[indexPath.row]
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return itemArray.count
+        }
         
-        cell.textLabel?.text = item.title
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+            
+            let item = itemArray[indexPath.row]
+            
+            cell.textLabel?.text = item.title
+            
+            //Ternary operator ==>
+            // value = condition ? valueIfTrue : valueIfFalse
+            
+            cell.accessoryType = item.done ? .checkmark : .none
+            
+            return cell
+        }
         
-        cell.accessoryType = item.done == true ? .checkmark : .none
+        //MARK: - TableView Delegate Methods
         
-        return cell
-    }
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
 
+    //        context.delete(itemArray[indexPath.row])
+    //        itemArray.remove(at: indexPath.row)
+            
+            itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 
-    //MARK: - TableView delegate methods
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        self.saveItems()
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    
-    //MARK: - Add new items
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        var textField = UITextField()
-        
-        let alert = UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Add item", style: .default) { (action ) in
-            // o que irá acontecer sempre que for clicado no + pra adicionar um item novo
+            saveItems()
             
-            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-            
-            let newItem = Item()
-            newItem.title = textField.text!
-            
-            self.itemArray.append(newItem)
-            
-            self.saveItems()
-        }
-        alert.addTextField{ (alertTextField) in
-            alertTextField.placeholder = "Create new items"
-            textField = alertTextField
+            tableView.deselectRow(at: indexPath, animated: true)
             
         }
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    //MARK: - Model Manipulation Methods
-    func saveItems() {
-        let encoder = PropertyListEncoder()
         
-        do{
-            try context.save()
-        } catch {
-            print("Error encoding array: \(error)")
-        }
+        //MARK: - Add New Items
         
-        self.tableView.reloadData()
-    }
-    
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do{
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("An erro was occurred \(error)")
+        @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+            
+            var textField = UITextField()
+            
+            let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+                //what will happen once the user clicks the Add Item button on our UIAlert
+                
+                
+                let newItem = Item(context: self.context)
+                newItem.title = textField.text!
+                newItem.done = false
+//                newItem.parentCategory = self.selectedCategory
+                self.itemArray.append(newItem)
+                
+                self.saveItems()
             }
+            
+            alert.addTextField { (alertTextField) in
+                alertTextField.placeholder = "Create new item"
+                textField = alertTextField
+                
+            }
+            
+            
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+            
         }
-    }
+        
+        //MARK - Model Manupulation Methods
+        
+        func saveItems() {
+            
+            do {
+              try context.save()
+            } catch {
+               print("Error saving context \(error)")
+            }
+            
+            self.tableView.reloadData()
+        }
+        
+        func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+            
+//            let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+//
+//            if let addtionalPredicate = predicate {
+//                request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, addtionalPredicate])
+//            } else {
+//                request.predicate = categoryPredicate
+//            }
+//
+//
+//            do {
+//                itemArray = try context.fetch(request)
+//            } catch {
+//                print("Error fetching data from context \(error)")
+//            }
+//
+//            tableView.reloadData()
+            
+        }
+    
+//    func loadItems() {
+//        let request : NSFetchRequest<Item> = Item.fetchRequest()
+//        do{
+//           itemArray =  try context.fetch(request)
+//        } catch {
+//            print("Error fetching data from context \(error)")
+//        }
+//    }
     
     
 }
